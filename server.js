df954854c8f00ceef2aeff4c694e52873f3c14d4
Kumari040 <kms745362@gmail.com>
2025-05-11 19:18:29 +0530

    Updated port for deploying

--- server.js
+++ server.js
@@ -24,4 +24,7 @@ app.post("/about-form",async function(req,res){
     }
 })
 
-app.listen(5500);
+const PORT = process.env.PORT || 5500;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
